ccb5c50f126d0ba70b02035daedab8b388ab03c2
h13maanb <h13maanb@PC349-14.ad.du.se>
2016-02-04 12:15:05 +0100

    index1

--- Twig-1.x/start/index1.php
+++ Twig-1.x/start/index1.php
@@ -1,31 +1,26 @@
 <?php
+/*
+error_reporting(E_ALL);
+ini_set('display_errors' 1);
 
-error_reporting(E_ERROR);
-ini_set('display_errors', 1);
- 
-
-include_once('../controllers/ControllerFactoryMethod.php');
-
-//läser av url:en efter (?)tecken i index1.php?PDOController/getCarByRegnr/ABC123
-//splittar den till en array så att jag kan kommat åt Controllern,metod och argument
-//$queryArray[0]=PDOController
-//$queryArray[1]=getCarByRegnr
-//$queryArray[2]=ABC123
+*/
+//inkluderar
+include_once '../controllers/Controller.php';
+//instansierar
+$controller = new Controller();
+//läser av url: en efter index.php?Productid/ABC123
+//splitter den till en array så att jag kan kommat åt metod resp arugment
 $queryArray = explode('/', $_SERVER['QUERY_STRING']);
-//skickar in Controller typen till fabriken som returnerar rätt controller instans antingen
-//en PDOController,en XMLController  eller En SPLFileController
-//$someController=ControllerFactoryMethod::getController($queryArray[0]);
-$someController=ControllerFactoryMethod::getController($queryArray[0]);
-
-//kollar så att metod finns i controllerobjektet, 
-//minskar risk för att någon försöker ändra i url genom att 
-//skicka in konstiga controller  och metodnamn
-if (method_exists($someController, $queryArray[1])) {
-   
-     $someController->$queryArray[1]($queryArray[2]);
-} else {
-    // vi har bara surfat in till index1.php eg ingen querystring   
-     $someController->getAllCars();
+//tex $controller->getProductid('ABC123');
+//kollar så att metod finns i objektet,
+//minskar risk för att någon försöker ändra i url genom att
+//skicka in konstiga metodnamn
+if  (metod_exist($controller, $queryArray[0])){
+        $controller->$queryArray[0]($queryArray[1]);
+}   else    {
+    // vi har bara surfat in till index1.php eg ingen querystring
+    $controller->getProductid('Dator')
 }
+    
 ?>
 
