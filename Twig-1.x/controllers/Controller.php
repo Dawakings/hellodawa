b311001cc9006b9f7ca32434e659aee282d91dc2
h14rtand <h14rtand@PC342-09.ad.du.se>
2016-02-09 11:37:38 +0100

    comm  klar sprint 1

--- Twig-1.x/controllers/Controller.php
+++ Twig-1.x/controllers/Controller.php
@@ -5,7 +5,7 @@ require_once '../models/Model.php';
 session_start();
 
 class Controller {
-    
+
     private $cart;
 
     public function getAllavaror() {
@@ -21,7 +21,7 @@ class Controller {
         //detta namn
         $template->display(array('varor' => $varorna));
     }
-    
+
     public function getKategories() {
         $modell = new Model();
         $varorna = $modell->getKategories();
@@ -35,100 +35,107 @@ class Controller {
         //detta namn
         $template->display(array('varor' => $varorna));
     }
-    
-    public function getinfoByKategori($kategori){
+
+    public function getinfoByKategori($kategori) {
         $modell = new Model();
         $varornaMain = $modell->getinfoByKategori($kategori);
-         $varorna = $modell->getKategories();
-         
-       Twig_Autoloader::register();
+        $varorna = $modell->getKategories();
+
+        Twig_Autoloader::register();
         // i vilken mapp finns templates:erna eg vyerna
-       $loader = new Twig_Loader_Filesystem('../templates/');
-       $twig = new Twig_Environment($loader);
+        $loader = new Twig_Loader_Filesystem('../templates/');
+        $twig = new Twig_Environment($loader);
         // laddar vyn som ska visa data om bilar
-       $template = $twig->loadTemplate('hemsida_ny.twig');
+        $template = $twig->loadTemplate('hemsida_ny.twig');
         //sätter data till variablen bilar som sedan är åtkomlig i vyn via
         //detta namn
-       $template->display(array('varornaMain' => $varornaMain, 'varor' => $varorna ));
-       
-  
-       
+        $template->display(array('varornaMain' => $varornaMain, 'varor' => $varorna));
+
+
+
         return $varornaMain;
     }
-    
-    public function getinfo($id){
+
+    public function getinfo($id) {
         $modell = new Model();
         $varornaMain = $modell->getInfo($id);
-         $varorna = $modell->getKategories();
-         
-       Twig_Autoloader::register();
+        $varorna = $modell->getKategories();
+
+        Twig_Autoloader::register();
         // i vilken mapp finns templates:erna eg vyerna
-       $loader = new Twig_Loader_Filesystem('../templates/');
-       $twig = new Twig_Environment($loader);
+        $loader = new Twig_Loader_Filesystem('../templates/');
+        $twig = new Twig_Environment($loader);
         // laddar vyn som ska visa data om bilar
-       $template = $twig->loadTemplate('hemsida_ny.twig');
+        $template = $twig->loadTemplate('hemsida_ny.twig');
         //sätter data till variablen bilar som sedan är åtkomlig i vyn via
         //detta namn
-       $template->display(array('varornaMain' => $varornaMain, 'varor' => $varorna ));
-       
-  
-       
+        $template->display(array('varornaMain' => $varornaMain, 'varor' => $varorna));
+
+
+
         return $varornaMain;
     }
-    
-    
+
     public function addtoCart($id) {
         if ($_SESSION['cart']) {
-            $this->cart = $_SESSION['cart'];
-            $kundvagnArray[0]=0;
+            $this->cart =  $_SESSION['cart'];
+            $kundvagnArray = $this->getinfo($id);
+
             if (!array_key_exists($id, $this->cart)) {
-                $this->cart[$id]=array($kundvagnArray[0],1);
+                $this->cart[$id] = array($kundvagnArray[0], 1);
+                $_SESSION['cart'] = $this->cart;
             } else {
-                $this->cart[$id][1]++;
-                $this->cart = $_SESSION['cart'];
+                $this->cart[$id][1] ++;
+                $_SESSION['cart'] = $this->cart;
             }
-        }
-    }
-    
-    public function deletefromCart($id) {
-        if ($_SESSION['cart']) {
-            $this->cart = $_SESSION['cart'];
             
-            if (array_key_exists($id, $this->cart)) {
-                $this->cart[$id][1]--;
-            }
-            if ($this->cart[$id][1] <= 0 )  {
-                unset($this->cart[$id]);
-            }
+        } 
+        else {
+            $_SESSION['cart'] = $this->cart;
+            $kundvagnArray = $this->getInfo($id);
+
+            $this->cart[$id] = array($kundvagnArray[0], 1);
+
             $_SESSION['cart'] = $this->cart;
         }
         
     }
-    
+
+    public function deletefromCart($id) {
+        if ($_SESSION['cart']) {
+            $this->cart = $_SESSION['cart'];
+
+            if (array_key_exists($id, $this->cart)) {
+                $this->cart[$id][1] --;
+            }
+            if ($this->cart[$id][1] <= 0) {
+                unset($this->cart[$id]);
+            }
+            $_SESSION['cart'] = $this->cart;
+        }
+        $this->showCart();
+    }
+
     public function belopp() {
         $belopp = 0;
         foreach ($_SESSION['cart'] as $attBetala) {
-            $belopp+=$attBetala[0]['pris']*$attBetala[1];
+            $belopp+=$attBetala[0]['pris'] * $attBetala[1];
         }
         return $belopp;
     }
-    
+
     public function showCart() {
-       Twig_Autoloader::register();
-       $loader = new Twig_Loader_Filesystem('../templates/');
-       $twig = new Twig_Environment($loader);
-       $template = $twig->loadTemplate('kundvagn.twig');
-       
-       $template->display(array('kundvagnen' => $_SESSION['cart'], 'attBetala' => $this->belopp() ));
+        Twig_Autoloader::register();
+        $loader = new Twig_Loader_Filesystem('../templates/');
+        $twig = new Twig_Environment($loader);
+        $template = $twig->loadTemplate('kundvagn.twig');
+
+        $template->display(array('kundvagnen' => $_SESSION['cart'], 'attBetala' => $this->belopp()));
     }
-    
-
-    
-
-  
-
 
 }
-/*$obj= new Controller();
 
-var_dump($obj->getinfoByKategori('Datorer')); */
+/*
+$obj= new Controller();
+
+var_dump($obj->addtoCart(0)); */
